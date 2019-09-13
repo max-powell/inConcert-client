0c6006d52ae1caf8d4d873fc58079a2410b6cdfa
max-powell <github email address>
2019-09-13 15:36:50 +0100

    created authorizing component to handle retrieval of access token

---
+++ src/containers/Authorizing.js
@@ -0,0 +1,82 @@
+import React, { Component } from 'react'
+import { Redirect } from 'react-router-dom'
+
+import { API_ROOT_URL } from '../constants'
+
+class Authorizing extends Component {
+
+  state = {
+    loading: true,
+    error: false,
+    access_token: ''
+  }
+
+  componentDidMount () {
+    const query = new URLSearchParams(window.location.search)
+    if (query.has('error')) {
+      return this.setState({
+        error: true,
+        loading: false
+      })
+    }
+
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      credentials: 'include',
+      body: query.toString()
+    }
+
+    fetch(`${API_ROOT_URL}/auth`, options)
+      .then(res => res.json())
+      .then(({access_token}) => {
+        this.setState({
+          access_token,
+          loading: false
+        })
+      })
+      .catch(() => this.setState({
+        error: true,
+        loading:false
+      }))
+  }
+
+  render() {
+    const { loading, error, access_token } = this.state
+
+    if (loading) {
+      return (
+        <div>Loading...</div>
+      )
+    }
+
+    if (error) {
+      return (
+        <Redirect
+          to={{
+            pathname: '/',
+            state: {
+              error: true
+            }
+          }}
+        />
+      )
+    }
+
+    return (
+      <Redirect
+        to={{
+          pathname: '/home',
+          state: {
+            access_token
+          }
+        }}
+      />
+    )
+  }
+
+}
+
+export default Authorizing
